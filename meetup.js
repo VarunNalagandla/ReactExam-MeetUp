f2360c98a1ef3d2669c94ece0b2c860907f30ff9
Nalagandla Varun <107249653+VarunNalagandla@users.noreply.github.com>
2025-06-11 16:20:34 +0530

    Update meetup.js

--- meetup.js
+++ meetup.js
@@ -1,19 +1,19 @@
 //RegisterContext.js
-
 import React from 'react'
 
 const HistoryContext = React.createContext({
-isRegister: false,
-changeRegistrationStatus: () => {},
-name: '',
-updateName: () => {},
-topic: '',
-updateTopic: () => {},
-registerErr: false,
-updateErr: () => {}
+  isRegister: false,
+  changeRegistrationStatus: () => {},
+  name: '',
+  updateName: () => {},
+  topic: '',
+  updateTopic: () => {},
+  registerErr: false,
+  updateErr: () => {},
 })
 
 export default HistoryContext
+
 
 //Home Component
 //index.js
@@ -21,151 +21,173 @@ export default HistoryContext
 import {Link} from 'react-router-dom'
 import HistoryContext from '../../Context/RegisterContext'
 import {
-MainDiv,
-BeforeRegisH1,
-BeforeRegisP,
-BeforeRegisBtn,
-BeforeRegisImg,
-AfterRegisImg,
-AfterRegisH1,
-AfterRegisP,
+  MainDiv,
+  BeforeRegisH1,
+  BeforeRegisP,
+  BeforeRegisBtn,
+  BeforeRegisImg,
+  AfterRegisImg,
+  AfterRegisH1,
+  AfterRegisP,
 } from './styledComponent'
-import NavBar from '../Navbar'
+import NavBar from '../NavBar'
+
 const Home = props => {
-const onClickRegister = () => {
-const {history) =props
-history.replace('/register')
+  const onClickRegister = () => {
+    const {history} = props
+    history.replace('/register')
+  }
+
+  const renderBeforeRegisteredView = () => (
+    <>
+      <BeforeRegisH1>Welcome to Meetup</BeforeRegisH1>
+      <BeforeRegisP>Please register for the topic</BeforeRegisP>
+      <Link to="/register">
+        <BeforeRegisBtn onClick={onClickRegister}>Register</BeforeRegisBtn>
+      </Link>
+      <BeforeRegisImg
+        src="https://assets.cchp.in/frontend/react-js/meetup/meetup-img.png"
+        alt="meetup"
+      />
+    </>
+  )
+
+  const renderAfterRegisteredView = (name, topic) => (
+    <>
+      <AfterRegisH1>{`Hello ${name}`}</AfterRegisH1>
+      <AfterRegisP>{`Welcome to ${topic}`}</AfterRegisP>
+      <AfterRegisImg
+        src="https://assets.ccbp.in/frontend/react-js/meetup/meetup-img.png"
+        alt="meetup"
+      />
+    </>
+  )
+
+  return (
+    <HistoryContext.Consumer>
+      {value => {
+        const {isRegister, name, topic} = value
+        return (
+          <>
+            <NavBar />
+            <MainDiv>
+              {isRegister
+                ? renderAfterRegisteredView(name, topic)
+                : renderBeforeRegisteredView()}
+            </MainDiv>
+          </>
+        )
+      }}
+    </HistoryContext.Consumer>
+  )
 }
-const renderBeforeRegisteredView = () => (
-<>
-<BeforeRegisH1>Welcome to Meetup</BeforeRegisH1>
-<BeforeRegisP>Please register for the topic</BeforeRegisP>
-<Link to="/register">
-<BeforeRegisBtn onclick={onClickRegister}>Register</BeforeRegisBtn>
-</Link>
-<BeforeRegisImg
-src="https://assets.cchp.in/frontend/react-js/meetup/meetup-ing.png"
-alt="meetup"
-/>
-</>
-)
-const renderAfterRegisteredView=(name, topic)=>(
-<>
-<AfterRegisH1>{`Hello $(name)`}</AfterRegisH1>
-<AfterRegisP>{`Welcome to ${topic}`}</AfterRegisP>
-<AfterRegisimg
-src="https://assets.ccbp.in/frontend/react-js/meetup/meetup-ing.png"
-alt-"meetup"
-/>
-</>
-)
-return (
-<HistoryContext.Consumer>
-(value=> (
-const {isRegister, name, topic}=value
-return(
-<>
-<NavBar />
-<MainDiv>
-{isRegister
-? renderAfterRegisteredView (name, topic)
-: renderBeforeRegisteredView()}
-</MainDiv>
-</>
-)
-}}
-</HistoryContext.Consumer>
-)
-}
+
 export default Home
 
-//styledComponents.js
+// styledComponents.js
 import styled from 'styled-components'
+
 export const MainDiv = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-min-height: 100vh;
-padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  padding: 20px;
 `
+
 export const BeforeRegisH1 = styled.h1`
-font-size: 50px;
-font-weight: 700;
-margin: 0px;
-color: #334155;
-margin-bottom: 15px;
+  font-size: 50px;
+  font-weight: 700;
+  margin: 0px;
+  color: #334155;
+  margin-bottom: 15px;
 `
+
 export const BeforeRegisP = styled.p`
-font-size: 24px;
-margin: 0px;
-font-weight: 700;
-margin-bottom: 25px;
-color: #475569;
+  font-size: 24px;
+  margin: 0px;
+  font-weight: 700;
+  margin-bottom: 25px;
+  color: #475569;
 `
+
 export const BeforeRegisBtn = styled.button`
-background-color: #2563eb;
-font-size: 18px;
-font-weight: 700;
-color: #ffffff;
-border: none;
-border-radius: 10px;
-height: 40px;
-width: 120px;
-margin-bottom: 25px;
-cursor: pointer;
+  background-color: #2563eb;
+  font-size: 18px;
+  font-weight: 700;
+  color: #ffffff;
+  border: none;
+  border-radius: 10px;
+  height: 40px;
+  width: 120px;
+  margin-bottom: 25px;
+  cursor: pointer;
 `
 
 export const BeforeRegisImg = styled.img`
-height: 350px;
-width: 60%;
+  height: 350px;
+  width: 60%;
 `
+
 export const AfterRegisImg = styled.img`
-height: 350px;
-width: 60%;
+  height: 350px;
+  width: 60%;
 `
+
 export const AfterRegisH1 = styled.p`
-font-size: 70px;
-font-weight: 700;
-color: #3b82f6;
-margin: 0px;
-margin-bottom: 10px;
+  font-size: 70px;
+  font-weight: 700;
+  color: #3b82f6;
+  margin: 0px;
+  margin-bottom: 10px;
 `
-export const AfterRegisp = styled.p`
-font-size: 34px;
-font-weight: 700;
-margin: 0px;
-margin-bottom: 25px;
-color: #334155;
+
+export const AfterRegisP = styled.p`
+  font-size: 34px;
+  font-weight: 700;
+  margin: 0px;
+  margin-bottom: 25px;
+  color: #334155;
 `
+
 
 //NavBar Component
 //index.js
 
+import {Navbar, Logo} from './styledComponent' // Adjust the path if necessary
+
 const NavBar = () => (
-<Navbar>
+  <Navbar>
     <Logo
-        src="https://assets.ccbp.in/frontend/react-js/meetup/website-logo-img."
-        alt="website logo"
+      src="https://assets.ccbp.in/frontend/react-js/meetup/website-logo-img.png"
+      alt="website logo"
     />
-</Navbar>
+  </Navbar>
 )
 
 export default NavBar
 
 //styledComponent.js
+import styled from 'styled-components'
 
 export const Navbar = styled.nav`
-display: flex;
-justify-content: flex-start;
-align-items: center;
-width: 100%;
-height: 70px;
-padding-left: 20px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+  padding-left: 20px;
+  box-sizing: border-box;
+  background-color: #ffffff;
 `
+
 export const Logo = styled.img`
-height: 80%;
+  height: 60%;
+  max-height: 50px;
+  object-fit: contain;
 `
+
 //NotFound Component
 //index.js
 
