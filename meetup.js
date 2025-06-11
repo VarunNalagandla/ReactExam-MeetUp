256dafc06993b0c76b2b6f72c583507241b9cfff
Nalagandla Varun <107249653+VarunNalagandla@users.noreply.github.com>
2025-06-11 14:37:49 +0530

    Update meetup.js

--- meetup.js
+++ meetup.js
@@ -166,4 +166,57 @@ padding-left: 20px;
 export const Logo = styled.img`
 height: 80%;
 `
+//NotFound Component
+//index.js
+
+import {
+NotFoundDiv,
+NotFoundImg,
+NotFoundH1,
+NotFoundP,
+} from './styledComponent'
+const NotFound = () => (
+<NotFoundDiv>
+<NotFoundImg
+src="https://assets.ccbp.in/frontend/react-js/meetup/not-found-img.png
+alt="not found"
+/>
+<NotFoundH1>Page Not Found</NotFoundH1>
+<NotFoundP>
+We are sorry, the page you requested could not be found
+</NotFoundP>
+</NotFoundDiv>
+)
+export default NotFound
+
+//styledComponent.js
+import styled from 'styled-components'
+
+export const NotFoundDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 20px;
+`
+
+export const NotFoundImg = styled.img`
+width: 50%;
+`
+
+export const NotFoundH1 = styled.h1`
+font-size: 34px;
+font-weight: 700;
+color: #334155;
+margin-bottom: 10px;
+`
+
+export const NotFoundP=styled.p`
+font-size: 24px;
+font-weight: 500;
+color: #7b8794;
+margin-bottom: 10px;
+`
+
+
 
